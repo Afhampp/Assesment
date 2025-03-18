9f589e1ec641f72d1a5487e853f5ff7fd325892c
Afhampp <afhamdunnurpp@gmail.com>
2025-03-18 15:38:30 +0530

    new

--- controller/markReader.js
+++ controller/markReader.js
@@ -2,7 +2,7 @@ const messageDB=require('../model/chatModel')
 
 const markreader=async(req,res)=>{
     const { message_id } = req.params;
-    const message = await Message.findByIdAndUpdate(message_id, { read: true }, { new: true });
+    const message = await messageDB.findByIdAndUpdate(message_id, { read: true }, { new: true });
   
     if (!message) {
       return res.status(404).json({ error: "Message not found" });
